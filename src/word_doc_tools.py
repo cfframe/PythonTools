658da58b69ef207efd197847c83f8d498ba7898d
ColinFF <3910544+cfframe@users.noreply.github.com>
2021-11-19 15:51:30 +0000

    Finish iterate_tables.  Separate out json_from_word_table. Add tests.

--- src/word_doc_tools.py
+++ src/word_doc_tools.py
@@ -17,39 +17,60 @@ from lxml import etree as ET
 class WordDocumentTools:
     """Utilities for handling Word Document files"""
 
+    WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+    PARA = WORD_NAMESPACE + 'p'
+    TEXT = WORD_NAMESPACE + 't'
+    TABLE = WORD_NAMESPACE + 'tbl'
+    ROW = WORD_NAMESPACE + 'tr'
+    CELL = WORD_NAMESPACE + 'tc'
+
     @staticmethod
-    def iterate_tables(document_path: str):
+    def iterate_tables(document_path: str) -> list:
+        """
 
+        :param document_path: str - path to Microsoft Word document containing table(s)
+        :return: list of json representations of tables
+        """
         # Start from https://newbedev.com/how-do-i-extract-data-from-a-doc-docx-file-using-python
-
-        WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
-        PARA = WORD_NAMESPACE + 'p'
-        TEXT = WORD_NAMESPACE + 't'
-        TABLE = WORD_NAMESPACE + 'tbl'
-        ROW = WORD_NAMESPACE + 'tr'
-        CELL = WORD_NAMESPACE + 'tc'
 
         print('File path: \n')
         print(document_path)
 
+        output = []
+
         with zipfile.ZipFile(document_path) as docx:
             document = docx.read('word/document.xml')
             tree = ET.XML(document)
-            # tree = xml.etree.ElementTree.XML(docx.read('word/document.xml'))
-            root = ET.fromstring(document)
-
-        count = 0
-        sub_count = 0
 
         # Avoid doubling up cells that are in tables nested within tables
-        for table in tree.iter(TABLE):
+        for table in tree.iter(WordDocumentTools.TABLE):
             table_parent = table.getparent()
-            if table_parent.tag != CELL and count < 3:
-                count += 1
-                print(f'TABLE-{count}-{table}----------------')
+            if table_parent.tag != WordDocumentTools.CELL:
+                output.append(WordDocumentTools.json_from_word_table(table))
 
-                for row in {row: row for row in table.iter(ROW) if row.getparent().getparent().tag != CELL}:
-                    for cell in {cell: cell for cell in row.iter(CELL)
-                                 if cell.getparent().getparent().getparent().tag != CELL}:
-                        print(''.join(node.text for node in cell.iter(TEXT)))
+        return output
+
+    @staticmethod
+    def json_from_word_table(table) -> str:
+        """Return json representation of a Microsoft Word table that is represented as an lxml.Element.
+        Tables nested in a cell are compressed to bare text and are not treated as separate tables.
+
+        :param table: input Microsoft Word table
+        :return str: json representation
+        """
+        datalist = []
+        for row in {row: row[0] for row in table.iter(WordDocumentTools.ROW)
+                    if row.getparent().getparent().tag != WordDocumentTools.CELL}:
+            row_data = []
+            for cell in {cell: cell for cell in row.iter(WordDocumentTools.CELL)
+                         if cell.getparent().getparent().getparent().tag != WordDocumentTools.CELL}:
+                # print(''.join(node.text for node in cell.iter(TEXT)))
+                row_data.append(''.join(node.text.strip() for node in cell.iter(WordDocumentTools.TEXT)))
+            datalist.append(row_data)
+
+        df = pd.DataFrame(datalist)
+        # Reset column headers to first row
+        df = df.rename(columns=df.iloc[0]).drop(df.index[0]).reset_index(drop=True)
+
+        return pd.DataFrame.to_json(df)
 
--- tests/word_doc_tools_test_case.py
+++ tests/word_doc_tools_test_case.py
@@ -6,8 +6,10 @@ import numpy as np
 import os
 import shutil
 import unittest
+import zipfile
 
 from pathlib import Path
+from lxml import etree as ET
 from src.file_tools import FileTools
 from src.word_doc_tools import WordDocumentTools
 
@@ -24,11 +26,69 @@ class WordDocumentToolsTestCase(unittest.TestCase):
         """Fixtures used by tests."""
         self.Root = Path(__file__).parent
 
-        self.TestWordDocPath = os.path.join(self.Root, 'TestWordDoc.docx')
+        self.TestWordDocSimpleTablePath = os.path.join(self.Root, 'TestWordDocSimpleTable.docx')
+        self.TestWordDocTableAcrossPagePath = os.path.join(self.Root, 'TestWordDocTableAcrossPage.docx')
+        self.TestWordDocNestedTablePath = os.path.join(self.Root, 'TestWordDocNestedTable.docx')
+        self.TestWordDocThreeTablesPath = os.path.join(self.Root, 'TestWordDocThreeTables.docx')
+        self.TableJsonSimple = '{"Field":{"0":"SimpleId","1":"AField","2":"AnotherField"},"Description":{"0":"Unique ' \
+                               'identifier.","1":"Stuff","2":"More stuff"},"Typeofvariable":{"0":"INT","1":"CHAR 30",' \
+                               '"2":"CHAR 30"}}'
+        self.TableJsonAcrossPage = \
+            '{"Fieldname":{"0":"LongTableId","1":"Field2","2":"Field3","3":"Field4","4":"Field5","5":"Field6",' \
+            '"6":"Field7","7":"Field8"},"Description":{"0":"Long table ID","1":"Dummy text. Video provides a powerful ' \
+            'way to help you prove your point. When you click Online Video, you can paste in the embed code for the ' \
+            'video you want to add.","2":"More dummy text.Type of episode . This value can be : \\u201c1\\u201d,' \
+            '\\u201d2\\u201d or \\u201d3\\u201d .Video provides a powerful way to help youprove your point. When you ' \
+            'click Online Video, you can paste in the embed code for the video you want to add.","3":"Dummy text.",' \
+            '"4":"Dummy text.","5":"Dummy text. Numbers starting by \\u2018000000001\\u2019.","6":"Even more dummy ' \
+            'text.","7":"Quick admission indicator. Can be:\\u201cX\\u201d for Yes.\\u201c\\u201d for No."},' \
+            '"Typeofvariable":{"0":"INT","1":"CHAR  10","2":"CHAR  1","3":"CHAR  10","4":"CHAR  6","5":"CHAR  9",' \
+            '"6":"CHAR  1","7":"CHAR  1"}}'
+
+        self.TableJsonHasSubTable = \
+            '{"Fieldname":{"0":"TableWithSubId","1":"SubTableInDescription","2":"SpaceToTrim","3":"AnotherField"},' \
+            '"Description":{"0":"Unique identifier. Number starting by \\u20181\\u2019.","1":"Gender identifier.M = ' \
+            'MaleU = unknownF = Female","2":"Surname1.","3":"Middlename."},"Typeofvariable":{"0":"INT","1":"CHAR 1",' \
+            '"2":"CHAR 30","3":"CHAR 30"}}'
+
         self.dummy = 'dummy'
 
-    def test_iterate_tables(self):
+    @staticmethod
+    def get_first_table_helper(document_path: str) -> ET.Element:
+        """ Unable at present to reasonably easily directly generate an lxml Element object that has a table.
+        This helper loads a test document which should have at least one table and returns the first table.
 
-        WordDocumentTools.iterate_tables(self.TestWordDocPath)
-        self.fail()
+        :param document_path: Path to a Word document
+        :return Element: table
+        """
+        with zipfile.ZipFile(document_path) as docx:
+            document = docx.read('word/document.xml')
+            tree = ET.XML(document)
 
+        for table in tree.iter(WordDocumentTools.TABLE):
+            return table
+
+        return None
+
+    def test_json_from_word_table__simple_table(self):
+        expected = self.TableJsonSimple
+        test_table = self.get_first_table_helper(self.TestWordDocSimpleTablePath)
+        actual = WordDocumentTools.json_from_word_table(test_table)
+        self.assertTrue(expected == actual)
+
+    def test_json_from_word_table__table_across_page(self):
+        expected = self.TableJsonAcrossPage
+        test_table = self.get_first_table_helper(self.TestWordDocTableAcrossPagePath)
+        actual = WordDocumentTools.json_from_word_table(test_table)
+        self.assertTrue(expected == actual)
+
+    def test_json_from_word_table__handle_sub_table(self):
+        expected = self.TableJsonHasSubTable
+        test_table = self.get_first_table_helper(self.TestWordDocNestedTablePath)
+        actual = WordDocumentTools.json_from_word_table(test_table)
+        self.assertTrue(expected == actual)
+
+    def test_iterate_tables__doc_with_three_top_level_tables__returns_list_of_three(self):
+        expected = 3
+        actual = len(WordDocumentTools.iterate_tables(self.TestWordDocThreeTablesPath))
+        self.assertTrue(expected == actual)

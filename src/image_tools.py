0f223f2330b8353cbf03feab9d40a96e497769c3
ColinFF <3910544+cfframe@users.noreply.github.com>
2022-01-27 10:34:15 +0000

    Add pad_images_array_to_aspect_ratio_reflect, image_corner_colours and are_image_corners_black to image_tools.py.

--- src/image_tools.py
+++ src/image_tools.py
@@ -9,6 +9,68 @@ class ImageTools:
     """Utilities for manipulating images"""
 
     @staticmethod
+    def pad_images_array_to_aspect_ratio_reflect(images: np.array, aspect_ratio: tuple) -> np.array:
+        """Reshape rectangle with padding based on image edges using reflection. Assume colour images.
+
+        :param images: array of images
+        :param aspect_ratio: aspect ratio (x, y) of padded image using Cartesian coordinates
+        :return: array of padded images
+        """
+
+        images = np.asarray(images, dtype='uint8')
+
+        yi, xi = images.shape[-3], images.shape[-2]
+
+        xj, yj = aspect_ratio
+
+        new_images = []
+        swap_axes = False
+        to_process_images = False
+        if float(yi)/xi > float(yj)/xj:
+            # Square or vertical rectangle to horizontal rectangle
+            # or vertical rectangle to square
+            to_process_images = True
+        elif float(yi)/xi < float(yj)/xj:
+            # Square or horizontal rectangle to vertical rectangle
+            # or horizontal rectangle to square
+            swap_axes = True
+            to_process_images = True
+            # Swap x and y dimensions
+            images = np.transpose(images, axes=(0, 2, 1, 3))
+            xi, yi = yi, xi
+            xj, yj = yj, xj
+
+        if to_process_images:
+            yk = yi
+            xk = (xj * yi) // yj
+            delta_x = xk - xi
+
+            for img in images:
+                add_1 = img[:, 0, :]
+                add_2 = img[:, -1, :]
+
+                # Accommodate odd-numbered delta_x with xk - xi - delta_x // 2. Could end up with opposite sides
+                # being appended with a difference of one strip of pixels
+                axis = 1
+                add_1n = np.tile(add_1[:, np.newaxis, :], (1, delta_x // 2, 1))
+                add_2n = np.tile(add_2[:, np.newaxis, :], (1, xk - xi - delta_x // 2, 1))
+
+                new_image = np.concatenate(
+                    (add_1n,
+                     img,
+                     add_2n),
+                    axis=axis)
+                new_images.append(new_image)
+
+            if swap_axes:
+                new_images = np.transpose(new_images, axes=(0, 2, 1, 3))
+        else:
+            # No change, so return original images
+            new_images = images
+
+        return new_images
+
+    @staticmethod
     def pad_images_array_to_aspect_ratio(images: np.array, aspect_ratio: tuple) -> np.array:
         """Reshape rectangle with padding based on image edges. Assume colour images.
 
@@ -197,3 +259,24 @@ class ImageTools:
             rotated_images.append(rot_image)
 
         return rotated_images
+
+    @staticmethod
+    def image_corner_colours(pil_image) -> list:
+        np_image = [np.array(pil_image)]
+        width, height = pil_image.size
+        pixels = pil_image.getdata()
+        # Corners at (0, 0), (width, 0), (0, height), (width, height). Allow for zero based coordinates
+        corners = [pixels[0], pixels[width - 1], pixels[width * (height - 1)], pixels[width * height - 1]]
+
+        return corners
+
+    @staticmethod
+    def are_image_corners_black(pil_image) -> bool:
+        # Arbitrary choice of rgb values of <10 being considered as black
+        colours = ImageTools.image_corner_colours(pil_image)
+        are_black = True
+        for colour in colours:
+            are_black = are_black and colour[0] < 10 and colour[1] < 10 and colour[2] < 10
+
+        return are_black
+
